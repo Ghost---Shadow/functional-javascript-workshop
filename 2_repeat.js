5ab9b44e33f7bb7ba0a0e0883bbad609a46625d8
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-16 10:01:48 +0530

    Zero repeat

--- 2_repeat.js
+++ 2_repeat.js
@@ -13,3 +13,10 @@ module.exports = repeat
     repeat(function () { i++; }, 10);
     console.log('Num increment test:\t', i === 10);
 }
+
+// Zero repeat test
+{
+    let i = 0;
+    repeat(function () { i++; }, 0);
+    console.log('Zero repeat test:\t', i === 0);
+}
