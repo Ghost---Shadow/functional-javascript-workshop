f24f96a4a2f07e46295ed4fd345f3bd2d0c8e26f
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-16 10:12:52 +0530

    Double array ideal case

---
+++ 3_double_array.js
@@ -0,0 +1,18 @@
+function doubleAll(numbers) {
+    return numbers.map(function (element) {
+        return element * 2;
+    });
+}
+
+module.exports = doubleAll;
+
+// Ideal case
+{
+    let a = [1, 2, 3];
+    let a_copy = a.slice();
+    a = doubleAll(a);
+    residue = a.filter(function (element, key) {
+        return a[key] === 2 * a_copy[key];
+    });
+    console.log('Ideal case test:\t', a.length == residue.length);
+}
