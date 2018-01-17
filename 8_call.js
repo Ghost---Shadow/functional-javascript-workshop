3648b143f333d3b71b9703cffad9a0c28a6e40f7
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-17 12:38:29 +0530

    8 null case

--- 8_call.js
+++ 8_call.js
@@ -1,5 +1,6 @@
 function duckCount() {
   return Array.prototype.slice.call(arguments)
+    .filter(obj => obj) // remove null objects
     .filter(obj => Object.prototype.hasOwnProperty.call(obj, 'quack'))
     .length;
 }
@@ -11,4 +12,11 @@ module.exports = duckCount;
   const notDuck = Object.create({ quack: true });
   const duck = { quack: true };
   console.log('Ideal case:', duckCount(duck, notDuck) === 1);
+}
+
+// Null case
+{
+  const obj1 = null;
+  const obj2 = null;
+  console.log('Ideal case:', duckCount(obj1, obj2) === 0);
 }
